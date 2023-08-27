eed734918db1c1f50b6c610e390ae1cb432ad2cb
Shady Mohamed <142973431+shadymohamedabdo@users.noreply.github.com>
2023-08-27 10:34:41 +0300

    Update hossam.dart

--- hossam.dart
+++ hossam.dart
@@ -2,7 +2,7 @@
 import 'dart:io';
 import 'package:hossam/hossam.dart' as hossam;
 void main() {
-  /// task 3 (Take a list and write a program that prints out all the elements of the list that are less than 5)
+  /// task 4 (Take a list and write a program that prints out all the elements of the list that are less than 5)
 
   List <int> x = [3,2,4,7,9,1,4,8];
   for (var i in x ){
